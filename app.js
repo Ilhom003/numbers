2f0c5e1e68da221f0e4ae78cbf2ee038e3b73374
ilhom003 <abdulahatov03gmail.com>
2023-01-31 20:34:35 +0500

    numbers

---
+++ app.js
@@ -0,0 +1,32 @@
+// console.log(100_000_000);
+
+// console.log(1e6);
+
+// let num = 0.1 + 0.2;
+// console.log(Number(num.toFixed(1)));
+
+// let num = 3.3242343;
+// console.log(+num.toPrecision(3));
+
+// console.log(Math.ceil(10.01));
+// console.log(Math.floor(10.01));
+// console.log(Math.trunc(10.01));
+// console.log(Math.random() * 10);
+
+
+// console.log(Math.min(1,2,3,4,5,4));
+
+// let num =5;
+// console.log(num.toString(3));
+
+
+
+//  Homework
+// console.log(Math.max(1, 2, 3, 4, 5, 4, 8) - Math.min(1, 2, 3, 4, 5, 4));
+
+// function randomIntiger(min, max) {
+
+//     return Math.trunc(Math.random() * (max - min))
+// }
+
+// console.log(randomIntiger(1, 50));
